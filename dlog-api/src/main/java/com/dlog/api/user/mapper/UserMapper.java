65124573d7facb95a7b6b03179e807df6071b94b
d-joom <106900072+d-joom@users.noreply.github.com>
2023-10-11 09:09:54 +0900

    Update UserMapper.java

--- dlog-api/src/main/java/com/dlog/api/user/mapper/UserMapper.java
+++ dlog-api/src/main/java/com/dlog/api/user/mapper/UserMapper.java
@@ -8,13 +8,14 @@ import org.mapstruct.factory.Mappers;
 import com.dlog.api.user.Dto.UserDto;
 import com.dlog.api.user.model.User;
 
-@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+@Mapper(componentModel = "spring", uses = {},
+nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.RETURN_DEFAULT,
 nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface UserMapper  {
 	
 	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 	
-	User toEntity(UserDto userDTO);
+	User toEntity(@MappingTarget User user, UserDto userDTO);
 	
-	UserDto toDto(User userEntity);
+	UserDto toDto(@MappingTarget UserDto usetDto, User user);
 }
