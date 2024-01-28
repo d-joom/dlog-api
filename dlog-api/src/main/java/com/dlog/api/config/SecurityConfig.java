3a2a95c0b20dbaae59d71a7c406394e2d65b8eb4
joomni <jming6371@gmail.com>
2024-01-28 14:34:53 +0900

    cors 설정 변경

--- dlog-api/src/main/java/com/dlog/api/config/SecurityConfig.java
+++ dlog-api/src/main/java/com/dlog/api/config/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.dlog.api.service.user.UserService;
 import com.dlog.api.utils.JwtTokenFilter;
@@ -21,6 +24,7 @@ public class SecurityConfig {
 	private final UserService userService;
     private static String secretKey = "my-secret-key-123123";
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -33,5 +37,16 @@ public class SecurityConfig {
                 .antMatchers("/login/info").authenticated()
 //                .antMatchers("/login/admin/**").hasAuthority(UserRole.ADMIN.name())
                 .and().build();
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
